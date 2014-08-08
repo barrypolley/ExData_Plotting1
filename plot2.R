e1ef06701fccff97d078e86d1845d66becc0016c
Barry Polley <barryp@well.com>
2014-08-08 20:48:36 +1200

    plot2.R

--- plot2.R
+++ plot2.R
@@ -1,13 +1,14 @@
-# Load data and generate first plot of assignment
+# Load data and generate second plot of assignment
 
 # Assumes the data file resides in the same directory as this script
 # Also assumes package sqldf and dependencies are available/installed
 
-# read data for two days and coerce types as necessary
+# read data for two days - merge date and time fields for X axis
 library(sqldf)
-# tbl1 <- read.csv.sql("household_power_consumption.txt", sql= "select * from file where Date='1/2/2007' or Date='2/2/2007'", header=TRUE, sep=";")
+tbl1 <- read.csv.sql("household_power_consumption.txt", sql= "select * from file where Date='1/2/2007' or Date='2/2/2007'", header=TRUE, sep=";")
+tbl1$FullDate <- strptime(paste(tbl1$Date,tbl1$Time), "%d/%m/%Y %H:%M:%S")
 
 # draw the plot to a .png file
-# png("plot1.png", width=480, height=480)
-hist(tbl1$Global_active_power, col="red", breaks=12, main="Global Active Power", ylim = c(0,1200), xlab="Global Active Power (kilowatts)")
-# dev.off()
+png("plot2.png", width=480, height=480)
+plot(tbl1$FullDate, tbl1$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
+dev.off()
